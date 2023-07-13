66736e750243029da709db49a0039c82e75a965d
cawyse9 <cawyse9@gmail.com>
2023-07-13 20:41:26 +0100

    update file paths to MU home drive

--- Data Processing/ABCD_5.0_common_core_variables_040723.R
+++ Data Processing/ABCD_5.0_common_core_variables_040723.R
@@ -15,14 +15,14 @@ library(psych)
 library(skimr)
 
 # master table starts with ID only - saved as csv file on server ABCD folder
-ABCD_all_ID <- read.csv("C:/Users/cwyse/OneDrive - Maynooth University/General/Research/ABCD/June Data Package CAW/ABCD_all_ID.csv")
+ABCD_all_ID <- read.csv("C:/Users/cwyse/OneDrive - Maynooth University/maynooth 2023/ABCD_Release_5_Core/study documents/ABCD_all_ID.csv")
 
 # rename this dataframe to start your own dataset
 core <- ABCD_all_ID
 core <- distinct(core) 
 
 # recursive list of files in release 5.0 folder - all tables
-allfiles <- list.files("C:/Users/cwyse/OneDrive - Maynooth University/General/Research/ABCD/June Data Package CAW/Release 5.0", full.names = T, recursive = T, pattern = ".*.csv")
+allfiles <- list.files("C:/Users/cwyse/OneDrive - Maynooth University/maynooth 2023/ABCD_Release_5_Core/Release 5.0", full.names = T, recursive = T, pattern = ".*.csv")
 
 # function that can retrieve variables from the tables store in ABDC release 5.0 folder on server and copy them to another table, either "core" line 24 above, or merge to existing ABCD table
 # define the table and variable you need here, var can be a vector
@@ -34,7 +34,7 @@ mydata <- core                    # the dataframe to merge the new variables
 getvar <- function(table_to_find, var_to_find, mydata){
 
     # open the file and get the variable and merge with master by ID and event
-    myfile <- list.files("C:/Users/cwyse/OneDrive - Maynooth University/General/Research/ABCD/June Data Package CAW/Release 5.0", full.name=T, recursive = TRUE, pattern=table_to_find)
+    myfile <- list.files("C:/Users/cwyse/OneDrive - Maynooth University/maynooth 2023/ABCD_Release_5_Core/Release 5.0", full.name=T, recursive = TRUE, pattern=table_to_find)
     myfile <- fread (myfile)
     
     ABCDdata <<- myfile %>%
